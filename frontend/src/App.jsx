792dc7cc6331bd2e2b0f5a7fd024fe4a0a333f1b
Akash Choudhury <ezdecode@gmail.com>
2024-10-26 23:01:25 +0530

    ..

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+        </ul>
+      </nav>
+    </header>
+    </>
+  )
+}
+
+export default App
