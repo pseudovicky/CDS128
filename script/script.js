5c186a57f903ea967b75421da511ecd533511999
Akash Choudhury <ezdecode@gmail.com>
2024-10-27 04:49:38 +0530

    Fix

--- script/script.js
+++ script/script.js
@@ -8,3 +8,17 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+gsap.to("#nav", {
+  backgroundColor: "rgba(0, 0, 0, 0.532)",
+  duration: 0.5,
+  height: "4.25rem",
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -80",
+    end: "top -70",
+    scrub: 3,
+  },
+});
